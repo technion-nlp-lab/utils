3c6bc365b8064fa165788347b070792a0d5a085f
Nadav Oved <nadavo@users.noreply.github.com>
2019-08-01 13:16:03 +0300

    Create get_free_gpu_list.py

---
+++ get_free_gpu_list.py
@@ -0,0 +1,23 @@
+from subprocess import Popen, PIPE
+import torch
+
+def get_free_gpu_list():
+  CUDA_VISIBLE_DEVICES = os.environ.get('CUDA_VISIBLE_DEVICES')
+  if CUDA_VISIBLE_DEVICES:
+    del os.environ['CUDA_VISIBLE_DEVICES']
+  free_gpu_list = list()
+  if torch.cuda.is_available():
+    gpu_output = Popen(["nvidia-smi", "-q", "-d", "PIDS"], stdout=PIPE, text=True)
+    gpu_processes = Popen(["grep", "Processes"], stdin=gpu_output.stdout, stdout=PIPE, text=True)
+    gpu_output.stdout.close()
+    processes_output = gpu_processes.communicate()[0]
+    for i, line in enumerate(processes_output.strip().split("\n")):
+      if line.endswith("None"):
+          print(f"Found Free GPU ID: {i}")
+          cuda_device = f"cuda:{i}"
+          free_gpu_list.append(torch.device(cuda_device))
+    if free_gpu_list:
+      os.environ['CUDA_VISIBLE_DEVICES'] = ",".join([str(gpu.index) for gpu in free_gpu_list])
+    else:
+      print("WARN - No Free GPUs found!")
+  return free_gpu_list, CUDA_VISIBLE_DEVICES
